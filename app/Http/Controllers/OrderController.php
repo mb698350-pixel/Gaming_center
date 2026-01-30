0427ca45c709b0a6b024968a0700153d09d2d68b
Mohammad_Bahrami <Mb698350@gmail.com>
2026-01-30 23:23:13 +0330

    edit structurc code in order unit in project

--- app/Http/Controllers/OrderController.php
+++ app/Http/Controllers/OrderController.php
@@ -43,10 +43,25 @@ class OrderController extends Controller
     public function show_order(){
         $user=Auth::user();
         $order = Order::where('user_id', $user->id)
-        ->where('status')
+        ->where('status','pending')
+        ->first()
         ->with('products');
 
-    return view('orders.order_show', compact('order'));
+        if (!$order){
+            return view('orders.order_show')->with('danger','شما سفارشی فعالی ندارید!!');
+        }
+
+        $order_item=$order->products;
+dd($order_item);
+    //     foreach($order_item as $item){
+
+    //         $item_quantity=$item->pivot->quantity;
+    //     }
+    // return view('orders.order_show', [
+    //     'user'=>$user,
+    //     'order'=>$order,
+    //     'item_quantity'=>$item_quantity,
+    // ]);
 
     }
 }
--- resources/views/orders/order_show.blade.php
+++ resources/views/orders/order_show.blade.php
@@ -9,24 +9,25 @@
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 text-gray-900 dark:text-gray-100">
-                   <h3>{{'show_order'}}</h3>
+                   <h3>{{'Show_order'}}</h3>
+                   <h3>{{'سفارش های کاربر'.$user->name}}</h3>
                     </div>
                     <h5>{{'Products'}}</h5>
 
-<table class="table">
-  <thead>
-    <tr>
-      <th scope="col"></th>
-      <th scope="col">نام</th>
-      <th scope="col">دسته بندی</th>
-      <th scope="col">قیمت</th>
-      <th scope="col">تنظیمات</th>
-    </tr>
-  </thead>
-  <tbody>
-{{ dd($order->products)}}
-  </tbody>
-</table>
+                        <table class="table">
+                          <thead>
+                            <tr>
+                              <th scope="col"></th>
+                              <th scope="col">نام</th>
+                              <th scope="col">دسته بندی</th>
+                              <th scope="col">تعداد سفارش</th>
+                              <th scope="col">قیمت</th>
+                              <th scope="col">تنظیمات</th>
+                            </tr>
+                          </thead>
+                          <tbody>
+                          </tbody>
+                        </table>
                 </div>
     
             </div>
