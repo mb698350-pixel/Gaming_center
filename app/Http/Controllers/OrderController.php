82ff95aee9879dad4484127d9bd3e7033a5e5a58
Mohammad_Bahrami <Mb698350@gmail.com>
2026-01-31 12:32:57 +0330

    add show products in order section

--- app/Http/Controllers/OrderController.php
+++ app/Http/Controllers/OrderController.php
@@ -42,26 +42,19 @@ class OrderController extends Controller
 
     public function show_order(){
         $user=Auth::user();
-        $order = Order::where('user_id', $user->id)
-        ->where('status','pending')
-        ->first()
-        ->with('products');
+        $order = Order::with(['products.category']) 
+        ->where('user_id', $user->id)
+        ->where('status', 'pending')
+        ->first();
 
         if (!$order){
             return view('orders.order_show')->with('danger','شما سفارشی فعالی ندارید!!');
         }
 
-        $order_item=$order->products;
-dd($order_item);
-    //     foreach($order_item as $item){
-
-    //         $item_quantity=$item->pivot->quantity;
-    //     }
-    // return view('orders.order_show', [
-    //     'user'=>$user,
-    //     'order'=>$order,
-    //     'item_quantity'=>$item_quantity,
-    // ]);
+    return view('orders.order_show', [
+        'user'=>$user,
+        'order'=>$order,
+    ]);
 
     }
 }
--- resources/views/orders/order_show.blade.php
+++ resources/views/orders/order_show.blade.php
@@ -9,23 +9,47 @@
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 text-gray-900 dark:text-gray-100">
-                   <h3>{{'Show_order'}}</h3>
-                   <h3>{{'سفارش های کاربر'.$user->name}}</h3>
+                   <h3>{{'Show order'}}</h3>
+                   <h3>{{'the user '.$user->name.' order'}}</h3>
                     </div>
                     <h5>{{'Products'}}</h5>
 
                         <table class="table">
-                          <thead>
-                            <tr>
-                              <th scope="col"></th>
-                              <th scope="col">نام</th>
-                              <th scope="col">دسته بندی</th>
-                              <th scope="col">تعداد سفارش</th>
-                              <th scope="col">قیمت</th>
-                              <th scope="col">تنظیمات</th>
-                            </tr>
-                          </thead>
+                              <thead>
+                                    <tr>
+                                      <th scope="col"></th>
+                                      <th scope="col">نام</th>
+                                      <th scope="col">دسته بندی</th>
+                                      <th scope="col">تعداد سفارش</th>
+                                      <th scope="col">قیمت</th>
+                                      <th scope="col">تنظیمات</th>
+                                    </tr>
+                              </thead>
                           <tbody>
+
+                                @foreach($order->products as $item)
+                                    <tr>
+                                        <td></td>
+                                        <td>{{$item->name}}</td>
+                                        <td>{{$item->category->name}}</td>
+                                        <td>{{$item->pivot->quantity}}</td>
+                                        <td>{{$item->price}}</td>
+                                        <td>
+
+                                              <form action="" method="POST" style="display:inline;">
+                                                      @csrf
+                                                      @method('DELETE')
+                                                          <button type="submit" class="btn btn-danger btn-sm" onclick="return confirm('آیا از حذف محصول اطمینان دارید؟')">
+                                                              Delete
+                                                          </button>
+                                              </form>
+                                                    <a href="">
+                                                          <button class="btn btn-warning btn-sm">Edit</button>
+                                                    </a>
+
+                                        </td>
+                                    </tr>
+                                @endforeach
                           </tbody>
                         </table>
                 </div>
