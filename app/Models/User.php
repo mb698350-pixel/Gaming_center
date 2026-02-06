e50e5b33966b575b1a5b27418b4092cc95b08f3c
Mohammad_Bahrami <Mb698350@gmail.com>
2026-02-06 11:46:49 +0330

    add Permission and Role table and model ralation in project

--- app/Models/User.php
+++ app/Models/User.php
@@ -48,4 +48,7 @@ class User extends Authenticatable
     public function orders(){
         return $this->hasMany(Order::class);
     }
+    public function roles(){
+        return $this->belongsToMany(Role::class);
+    }
 }
